ac8298361cee3143969c7126b14ffac51d0fdb6c
zachczx <zixian.chen@gmail.com>
2025-11-21 08:14:55 +0800

    Changed icons to expressive ones

---
+++ src/lib/assets/expressive-icons/FluentEmojiFlatPill.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let props = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="1em"
+	height="1em"
+	viewBox="0 0 32 32"
+	{...props}
+	class={props.class}
+	><!-- Icon from Fluent Emoji Flat by Microsoft Corporation - https://github.com/microsoft/fluentui-emoji/blob/main/LICENSE --><g
+		fill="none"
+		><path
+			fill="#e7d4d9"
+			d="m21.415 21.405l-6.39 6.39a7.63 7.63 0 0 1-10.79 0a7.63 7.63 0 0 1 0-10.79l6.39-6.39l8.642 2.585z"
+		/><path
+			fill="var(--color-neutral)"
+			d="m10.635 10.625l6.39-6.39a7.63 7.63 0 0 1 10.79 0a7.63 7.63 0 0 1 0 10.79l-6.39 6.39z"
+		/><path fill="#F4F4F4" d="M26 12a2 2 0 1 0 0-4a2 2 0 0 0 0 4" /></g
+	></svg
+>
---
+++ src/lib/assets/expressive-icons/StreamlineColorHotelLaundryFlat.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let props = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="1em"
+	height="1em"
+	viewBox="0 0 14 14"
+	{...props}
+	class={[props.class, 'opacity-[0.4] hue-rotate-220']}
+	><!-- Icon from Streamline color by Streamline - https://creativecommons.org/licenses/by/4.0/ --><g
+		fill="none"
+		><path
+			fill="#8fbffa"
+			fill-rule="evenodd"
+			d="M2.5 0A1.5 1.5 0 0 0 1 1.5v11A1.5 1.5 0 0 0 2.5 14h9a1.5 1.5 0 0 0 1.5-1.5v-11A1.5 1.5 0 0 0 11.5 0z"
+			clip-rule="evenodd"
+		/><path fill="#2859c5" d="M2.5 0A1.5 1.5 0 0 0 1 1.5V3h12V1.5A1.5 1.5 0 0 0 11.5 0z" /><path
+			fill="#2859c5"
+			fill-rule="evenodd"
+			d="M7 4.892a3.607 3.607 0 1 0 0 7.215a3.607 3.607 0 0 0 0-7.215"
+			clip-rule="evenodd"
+		/><path
+			fill="#8fbffa"
+			fill-rule="evenodd"
+			d="M4.995 7.783a.5.5 0 0 0-.493.417a2.547 2.547 0 0 0 5.024.848a.5.5 0 0 0-.493-.582h-.497c-.753 0-1.134-.147-1.51-.304l-.06-.025c-.404-.167-.852-.354-1.617-.354z"
+			clip-rule="evenodd"
+		/></g
+	></svg
+>
