94f729d78993a52b881a843e89728ae387ea1b18
zachczx <zixian.chen@gmail.com>
2025-11-29 22:57:51 +0800

    feat: add error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,16 @@
 <script>
 	import { pb } from '$lib/pb';
 	import PageWrapper from '$lib/shell/PageWrapper.svelte';
+	import Oops from '$lib/assets/oops.webp?w=400;300&enhanced';
 </script>
 
-<PageWrapper title="Sundry" {pb}
-	><div class="grid h-full place-items-center">
+<PageWrapper title="Sundry" {pb}>
+	<div class="grid h-full content-center justify-items-center gap-4">
+		<enhanced:img
+			src={Oops}
+			alt="Error!"
+			sizes="(min-width:1920px) 400px, (min-width:100px) 300px"
+		/>
 		<h2 class="text-xl font-medium">Ugh, something's broken.</h2>
-	</div></PageWrapper
->
+	</div>
+</PageWrapper>
