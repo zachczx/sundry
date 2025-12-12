81302f826907862a2cc023166ed330d019c109c6
zachczx <zixian.chen@gmail.com>
2025-12-12 14:58:15 +0800

    feat: tweak feed dropdown UI

--- src/lib/ui/FeedDropdown.svelte
+++ src/lib/ui/FeedDropdown.svelte
@@ -21,7 +21,7 @@
 		/>
 	</div>
 	<ul
-		class="dropdown-content menu rounded-box bg-base-100 text-md text-base-content z-1 w-72 p-2 shadow-lg"
+		class="dropdown-content menu rounded-box bg-base-popover text-md text-base-content z-1 w-80 p-2 shadow-lg"
 	>
 		{#if !feedItems || feedItems?.items?.length === 0}
 			<li>
@@ -35,16 +35,10 @@
 			<div class="text-base-content/70 px-3 py-2 font-medium uppercase">Recent Actions</div>
 			{#each feedItems?.items as item}
 				{@const fromNow = dayjs(item.time).fromNow()}
-				<li class="border-b-base-content/10 border-b py-1 last:border-b-0">
-					<div class="flex items-center">
-						<div class="flex grow items-center gap-2">
-							<span class="font-semibold">{item.expand?.tracker?.display}</span
-							><MaterialSymbolsArrowRightAlt />
-							{fromNow}
-						</div>
-						<!-- <div>
-							<MaterialSymbolsChevronRight class="size-5 opacity-50" />
-						</div> -->
+				<li class="border-b-base-content/5 border-b py-1 last:border-b-0">
+					<div class="flex items-baseline gap-2">
+						<div class="font-semibold">{item.expand?.tracker?.display}</div>
+						<div class="text-base-content/70">{fromNow}</div>
 					</div>
 				</li>
 			{/each}
